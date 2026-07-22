84ce1adaef399ad63f7eaef12d8907b32b787cec
jalyn <jalynrose@icloud.com>
2026-07-22 14:26:11 -0400

    Added Person, Librarian, Address

---
+++ src/main/java/com/zipcodewilmington/centrallibrary/Librarian.java
@@ -0,0 +1,61 @@
+// extends means the librarian has everything from "person" file
+
+    public class Librarian extends Person {
+    private String employeeId;
+    private String department;
+    private double salary;
+    }
+  // Super is calling what we did in "person" file
+// super(name,age,email,phoneNumber)
+
+// constructor
+  public Librarian(String name, int age, String email, String phoneNumber,
+                 String employeeId, String department, double salary) {
+      super (name, age, email, phoneNumber);              
+                
+       this.employeeId = employeeId;
+       this.department = department;
+       this.salary = salary;        
+                
+    } 
+    
+
+//getters
+
+public String getEmployeeId() {
+    return employeeId;
+}
+
+public String getDepartment () {
+    return department;
+}
+
+public double getSalary () {
+    return salary;
+}
+
+//Setters
+
+public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+}
+
+public void setDepartment(String department) {
+        this.department = department;
+}
+
+public void setSalary(double salary) {
+        this.salary = salary;
+}
+
+//
+
+//Methods
+
+public void addItemToLibrary (){
+    // TODO: implement when library class is complete
+}
+
+public void removeItemToLibrary () {
+    // TODO: implement when library class is complete
+}
