9d6e02a76f8063bf4836e8de343f3810159c7faf
terrik <terrik_smith@yahoo.com>
2026-07-22 11:57:39 -0400

    TS-LibarayItemClassCreated-07222026

---
+++ src/main/java/com/zipcodewilmington/centrallibrary/LibraryItem.java
@@ -0,0 +1,47 @@
+package com.zipcodewilmington.centralibrary;
+import java.util.List;
+
+public abstract class LibraryItem implements Searchable {
+
+    private String id;
+    private String title;
+    private String location;
+    private boolean available;
+
+    // Constructor
+    public LibraryItem(String id, String title, String location) {
+        this.id = id;
+        this.title = title;
+        this.location = location;
+        this.available = true;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public boolean isAvailable() {
+        return available;
+    }
+    public void checkOut() {
+    available = false;
+    }
+    public void checkIn() {
+    available = true;
+    }
+    public abstract double calculateLateFee(int daysLate);
+
+    public abstract int getMaxBorrowDays();
+
+    public abstract String getItemType();
+
+    public abstract List<String> getSearchableFields();
+
+
+}
+
+
