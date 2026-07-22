337a79ccf6b903c4864acabe2cf910449279d590
terrik <terrik_smith@yahoo.com>
2026-07-22 14:52:39 -0400

    tryingtogetbranch

---
+++ src/main/java/com/zipcodewilmington/centrallibrary/Book.java
@@ -0,0 +1,15 @@
+package com.zipcodewilmington.centrallibrary;
+
+
+public class Book extends LibraryItem {
+private String author;
+private String isbn;
+private int pages;
+private String genre;
+private boolean reserved;
+private LibraryMember reservedBy;
+
+super(id, title, location);
+
+    
+}//hello
