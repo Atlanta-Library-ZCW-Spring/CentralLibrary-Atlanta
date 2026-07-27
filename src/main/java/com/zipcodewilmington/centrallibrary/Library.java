9649c60c5956003dd593f9fd88735bf00152fd41
terrik <terrik_smith@yahoo.com>
2026-07-27 11:01:13 -0400

    Completed Library

--- src/main/java/com/zipcodewilmington/centrallibrary/Library.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/Library.java
@@ -66,12 +66,61 @@ public class Library {
 
     public void displayAllItems() {
         for (LibraryItem item : items) {
+
+            String type = item.getItemType();
+            String title = item.getTitle();
+
+            String status;
+            if (item.isAvailable()) {
+                status = "Available";
+            } else {
+                status = "Checked Out";
+            }
+
+            System.out.println(type + " | " + title + " | " + status);
+        }
+    }
+
+    // Searching method
+    public List<LibraryItem> search(String keyword) {
+        List<LibraryItem> searchResults = new ArrayList<>();
+
+        int bookCount = 0;
+        int dvdCount = 0;
+        int periodicalCount = 0;
+        int musicCount = 0;
+
+        for (LibraryItem libraryItem : items) {
+
+            if (libraryItem.matchesKeyword(keyword)) {
+
+                if (libraryItem instanceof Book && bookCount < 10) {
+                    searchResults.add(libraryItem);
+                    bookCount++;
+                } else if (libraryItem instanceof DVD && dvdCount < 10) {
+                    searchResults.add(libraryItem);
+                    dvdCount++;
+                } else if (libraryItem instanceof Periodical && periodicalCount < 10) {
+                    searchResults.add(libraryItem);
+                    periodicalCount++;
+                } else if (libraryItem instanceof Music && musicCount < 10) {
+                    searchResults.add(libraryItem);
+                    musicCount++;
+                }
+            }
+        }
+
+        return searchResults;
+    }
+
+    public void generateLateFeeReport() {
+        for (LibraryMember checkedMember : members) {
             System.out.println(
-                    item.getItemType() + " | " +
-                    item.getTitle() + " | " +
-                    (item.isAvailable() ? "Available" : "Checked Out")
+                checkedMember.getName()
+                + " has $"
+                + checkedMember.getOutstandingFees()
+                + " in late fees."
             );
         }
     }
-    
 }
